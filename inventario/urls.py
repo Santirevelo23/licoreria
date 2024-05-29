333cb7c63b127919296dde351185aebbeb6c1187
Carlos <carlos.guerreroart@gmail.com>
2024-05-29 17:18:53 -0500

    final

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('book/', views.book, name='book'),
     path('create/product/', views.create_product, name='create_product'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
